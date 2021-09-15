27dbf29f3c46f95b576b3a545c1b96bfff3abccf
Daniele Scasciafratte <mte90net@gmail.com>
2021-09-15 16:37:36 +0200

    feat(install): silence

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ set -o errexit
 cd $(dirname $0)
 
 cd ./wp-hooks
-curl -O https://raw.githubusercontent.com/johnbillion/wp-hooks/master/hooks/actions.json
-curl -O https://raw.githubusercontent.com/johnbillion/wp-hooks/master/hooks/filters.json
+curl -O https://raw.githubusercontent.com/johnbillion/wp-hooks/master/hooks/actions.json > /dev/null
+curl -O https://raw.githubusercontent.com/johnbillion/wp-hooks/master/hooks/filters.json > /dev/null
